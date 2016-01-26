3875a7ca55a5580004cb102472311f3c8f110dc9
kroltan <kroltan1@gmail.com>
2016-01-26 16:02:52 -0200

    assembly info

--- AtmoSerialize/Properties/AssemblyInfo.cs
+++ AtmoSerialize/Properties/AssemblyInfo.cs
@@ -1,16 +1,15 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("AtmoSerialize")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Library to read and write .atmo files")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Troposphir")]
 [assembly: AssemblyProduct("AtmoSerialize")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright © Troposphir Team 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
