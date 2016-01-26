61963e9d749269701c89ca4e29447f2485796808
kroltan <kroltan1@gmail.com>
2016-01-26 16:01:15 -0200

    documented public methods, added README

--- AtmoSerialize/AtmoConvert.cs
+++ AtmoSerialize/AtmoConvert.cs
@@ -1,10 +1,25 @@
 ﻿using System;
 using System.IO;
 using System.Threading.Tasks;
+using ICSharpCode.SharpZipLib.Zip.Compression;
 using ICSharpCode.SharpZipLib.Zip.Compression.Streams;
 
 namespace AtmoSerialize {
+    /// <summary>
+    /// Provides serialization methods to write and read maps.
+    /// All methods are non-greedy, this means they expect the stream to be on
+    /// a position such as it immediately starts with the map data, and will not
+    /// consume any extra bytes from it, assuming a well-formed map.
+    /// 
+    /// These methods will not close the stream after reading or writing.
+    /// </summary>
     public static class AtmoConvert {
+        /// <summary>
+        /// Writes the contents of the map to the given stream.
+        /// </summary>
+        /// <param name="stream">The destination stream.</param>
+        /// <param name="map">The map to be serialized.</param>
+        /// <param name="compress">If true, will compress the map data.</param>
         public static void Serialize(Stream stream, Map map, bool compress) {
             var error = ValidateSerializeParameters(stream, map, compress);
             if (error != null) {
@@ -13,13 +28,20 @@ namespace AtmoSerialize {
 
             WriteHeader(stream, map, compress);
             if (compress) {
-                stream = new DeflaterOutputStream(stream);
+                stream = new DeflaterOutputStream(stream, new Deflater(Deflater.BEST_COMPRESSION));
             }
             var writer = new BinaryWriter(stream);
             map.SerializeContents(writer);
             writer.Flush();
         }
 
+        /// <summary>
+        /// Asynchronously writes the contents of the map to the given stream.
+        /// </summary>
+        /// <param name="stream">The destination stream.</param>
+        /// <param name="map">The map to be serialized.</param>
+        /// <param name="compress">If true, will compress the map data.</param>
+        /// <returns>A task that is completed once writing finishes.</returns>
         public static async Task SerializeAsync(Stream stream, Map map, bool compress) {
             var error = ValidateSerializeParameters(stream, map, compress);
             if (error != null) {
@@ -29,6 +51,11 @@ namespace AtmoSerialize {
             await Task.Run(() => Serialize(stream, map, compress));
         }
 
+        /// <summary>
+        /// Reads a <see cref="Map"/> instance from a stream.
+        /// </summary>
+        /// <param name="stream">A stream, positioned at the exact start of the header.</param>
+        /// <returns>The object representation of the Map.</returns>
         public static Map Deserialize(Stream stream) {
             var error = ValidateDeserializeParameters(stream);
             if (error != null) {
@@ -38,6 +65,11 @@ namespace AtmoSerialize {
             return Deserializer.ReadMap(stream);
         }
 
+        /// <summary>
+        /// Asynchronously reads a <see cref="Map"/> instance from a stream.
+        /// </summary>
+        /// <param name="stream">A stream, positioned at the exact start of the header.</param>
+        /// <returns>A task which resolves to the object representation of the Map.</returns>
         public static async Task<Map> DeserializeAsync(Stream stream) {
             var error = ValidateDeserializeParameters(stream);
             if (error != null) {
---
+++ README.md
@@ -0,0 +1,26 @@
+#AtmoSerialize
+
+A library to load and save Atmosphir `.atmo` files.
+
+Currently, it is very low level, with a bare serializer. This means that object properties (like rulebook and triggers) are not type-safe.
+
+
+##Examples
+
+```csharp
+using (var stream = File.OpenRead("draft.atmo")) {
+	var map = AtmoConvert.Deserialize(stream);
+	Console.WriteLine(map.Version);
+}
+
+using (var stream = File.OpenWrite("out.atmo")) {
+	var map = new Map(5);
+	var item = new Item("mi_flag_start", new Vector(0,0,0), new Vector(0,0,0), 1f);
+	map.Resources.Add(item.ResourceName);
+	map.Items.Add(item);
+
+	AtmoConvert.Serialize(stream, map, true); //boolean for compression
+}
+```
+
+Asynchronous versions of `AtmoConvert.Serialze` and `AtmoConvert.Deserialize` are also available.
